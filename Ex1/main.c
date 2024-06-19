7553b01eac71b9356a18c157727c912693441668
Almog Dobrescu <dobalmog123@gmail.com>
2024-06-19 13:27:21 +0200

    reading input

--- Ex1/main.c
+++ Ex1/main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #define MATRIX_IMPLEMENTATION
-#include "Matrix_Double.c"
+#include "Matrix_Double.h"  /* I included this library for debuging */
 #include <string.h>
+#include <errno.h>
 
 #define MAXDIR 100
+#define MAXWORD 100
 #define dprintSTRING(expr) printf(#expr " = %s\n", expr)    /* macro for easy debuging*/
 #define dprintINT(expr) printf(#expr " = %d\n", expr)
 #define dprintF(expr) printf(#expr " = %g\n", expr)
 #define dprintD(expr) printf(#expr " = %g\n", expr)
+
+void read_input(char *dir);
+void output_solution_d(char *dir, double *data);
+
+/* Input variables */
+double t, delta_y, XSF, YSF;
+int i_max, j_max, i_TEL, i_LE, i_TEU;
 
 int main(int argc, char const *argv[])
 {
@@ -31,10 +40,78 @@ int main(int argc, char const *argv[])
         return -1;
     }
     /*------------------------------------------------------------*/
+    read_input(input_dir);
+    /*------------------------------------------------------------*/
 
-    dprintSTRING(input_dir);
-    dprintSTRING(output_dir);
-
+    /* Checking that I got the right input */
+    dprintD(t);
+    dprintINT(i_max);
+    dprintINT(j_max);
+    dprintINT(i_TEL);
+    dprintINT(i_LE);
+    dprintINT(i_TEU);
+    dprintD(delta_y);
+    dprintD(XSF);
+    dprintD(YSF);
+    printf("--------------------\n");
+    /*------------------------------------------------------------*/
+    
+    Mat m = mat_alloc(3, 3);
+    mat_fill(m, 2);
+    MAT_PRINT(m);
 
     return 0;
+}
+
+/* sets 'flags' and variables according to the input file
+argument list: dir - the directory of the input file */
+void read_input(char *dir)
+{
+    FILE *fp = fopen(dir, "rt");
+    char current_word[MAXWORD];
+    float temp;
+
+    if (!fp) {
+        fprintf(stderr, "Error opening file: %s\n", strerror(errno));
+    }
+
+    /* Seting the input varibles according to the input file */
+    while(fscanf(fp, "%s", current_word) != EOF) {  
+        if (!strcmp(current_word, "t")) {
+            fscanf(fp, "%g", &temp); /* fscanf returns a float, so I cast it to double */
+            t = (double)temp;
+        } else if (!strcmp(current_word, "i_max")) {
+            fscanf(fp, "%d", &i_max);
+        } else if (!strcmp(current_word, "j_max")) {
+            fscanf(fp, "%d ", &j_max);
+        } else if (!strcmp(current_word, "i_TEL")) {
+            fscanf(fp, "%d", &i_TEL);
+        } else if (!strcmp(current_word, "i_LE")) {
+            fscanf(fp, "%d", &i_LE);
+        } else if (!strcmp(current_word, "i_TEU")) {
+            fscanf(fp, "%d", &i_TEU);
+        } else if (!strcmp(current_word, "delta_y")) {
+            fscanf(fp, "%g", &temp); /* fscanf returns a float, so I cast it to double */
+            delta_y = (double)temp;
+        } else if (!strcmp(current_word, "XSF")) {
+            fscanf(fp, "%g", &temp); /* fscanf returns a float, so I cast it to double */
+            XSF = (double)temp;
+        } else if (!strcmp(current_word, "YSF")) {
+            fscanf(fp, "%g", &temp); /* fscanf returns a float, so I cast it to double */
+            YSF = (double)temp;
+        }
+    }
+
+    fclose(fp);
+}
+
+/* output data; double version
+argument list: dir - the directory of the output file.
+data - the solution vector */
+void output_solution_d(char *dir, double *data)
+{
+    FILE *fp = fopen(dir, "wt");
+    
+
+    fclose(fp);
 }
