fb36f5f9126bcc6d3d1d91f75bbcd555bc2631fa
Almog Dobrescu <dobalmog123@gmail.com>
2024-10-21 14:47:20 +0300

    creating command_to_run.txt

--- Ex2/automat.c
+++ Ex2/automat.c
@@ -6,30 +6,56 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <stdarg.h>
 
 int create_empty_dir(char *parent_directory);
+void print_command_to_file(FILE *fp, char *program, ...);
 
 int main()
 {
+    char parent_dir[] = "./auto_results";
     if (create_empty_dir("./auto_results") != 0) {
         return 1;
     }
 
-    int id = fork();
-    if (id == 0) {
-        printf("running solver\n");
-        execlp("./solver",
-               "./solver",
-               "./input.txt",
-               "./mesh_output.txt",
-               "./auto_results",
-               "69",
-               NULL);
+    char temp_dir[BUFSIZ];
+    strncpy(temp_dir, parent_dir, BUFSIZ);
+    strncat(temp_dir, "/command_to_run.txt", BUFSIZ/2);
+    FILE *fp = fopen(temp_dir, "wt");
 
-    } else {
-        while (wait(NULL) != -1);
-        printf("finished\n");
-    }
+    fprintf(fp, "make build_solver\n");
+
+    print_command_to_file(fp,
+                          "./solver",
+                          "./input.txt",
+                          "./mesh_output.txt",
+                          "./auto_results",
+                          "70",
+                          NULL);
+    print_command_to_file(fp,
+                          "./solver",
+                          "./input.txt",
+                          "./mesh_output.txt",
+                          "./auto_results",
+                          "69",
+                          NULL);
+
+
+    // int id = fork();
+    // if (id == 0) {
+    //     printf("running solver\n");
+    //     execlp("./solver",
+    //            "./solver",
+    //            "./input.txt",
+    //            "./mesh_output.txt",
+    //            "./auto_results",
+    //            "70",
+    //            NULL);
+
+    // } else {
+    //     while (wait(NULL) != -1);
+    //     printf("finished\n");
+    // }
 
     return 0;
 }
@@ -50,6 +76,7 @@ int create_empty_dir(char *parent_directory)
             }
             struct dirent* entity;
             entity = readdir(dir);
+            printf("\n");
             while (entity != NULL) {   /* directory is not empty */
                 strncpy(path_to_remove, parent_directory, BUFSIZ);
                 strncat(path_to_remove, "/", BUFSIZ/2);
@@ -77,4 +104,30 @@ int create_empty_dir(char *parent_directory)
         return 1;
     }
     return 0;
+}
+
+void print_command_to_file(FILE *fp, char *program, ...)
+{
+    // printf("%s\n", program);
+
+    // va_list args;
+    // va_start(args, program);
+    // char *temp_string;
+    // while ((temp_string = va_arg(args, char *)) != NULL) {
+    //     printf("%s\n", temp_string);
+    // }
+    // printf("\n");
+
+    // va_end(args);
+
+    fprintf(fp, "%s ", program);
+    va_list args;
+    va_start(args, program);
+    char *temp_string;
+    while ((temp_string = va_arg(args, char *)) != NULL) {
+        fprintf(fp, "%s ", temp_string);
+    }
+    va_end(args);
+    fprintf(fp, "\n");
+
 }
